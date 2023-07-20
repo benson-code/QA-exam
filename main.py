9947d25d3cf407206fc457efa418a6a0dae64d1f
benson-code <78255359+benson-code@users.noreply.github.com>
2023-07-21 00:30:02 +0800

    test

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+def print_triangle(n):
+    for i in range(n):
+        for j in range((n - i) - 1):
+            print(" ", end="")
+        for k in range(i + 1):
+            if k == 0 or k == i or i == n - 1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+
+n = int(input("請輸入正空心三角形的邊長："))
+print_triangle(n)
